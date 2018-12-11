c0f9e821fca89c805f5da60c484d560f4783a968
ilhamsj <ilhamsaputrajati@gmail.com>
2018-12-11 21:33:21 +0700

    new table passports

--- database/migrations/2018_12_11_133435_create_passports_table.php
+++ database/migrations/2018_12_11_133435_create_passports_table.php
@@ -15,6 +15,12 @@ class CreatePassportsTable extends Migration
     {
         Schema::create('passports', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->integer('date');
+            $table->string('email')->unique();
+            $table->integer('number');
+            $table->string('office');
+            $table->string('filename');
             $table->timestamps();
         });
     }
