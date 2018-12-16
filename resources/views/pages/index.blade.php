c84b9176f9337a714c5120760978bb951c4f93b6
ilhamsj <ilhamsaputrajati@gmail.com>
2018-12-16 09:14:47 +0700

    create and store posts

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,18 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="jumbotron">
-        <h1 class="display-3">{{$title}}</h1>
-        <p class="lead">
-          Cras justo odio, dapibus ac facilisis in, egestas eget quam. 
-        </p>
-      </div>
+  <div class="jumbotron">
+      <h1 class="display-3">{{$title}}</h1>
+      <p class="lead">
+        Cras justo odio, dapibus ac facilisis in, egestas eget quam. 
+      </p>
+    </div>
 
-      <div class="row marketing">
-        <div class="col-lg-6">
-          <h4>Subheading</h4>
-          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-
-        </div>
+    <div class="row marketing">
+      <div class="col-lg-6">
+        <h4>Subheading</h4>
+        <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
       </div>
+    </div>
 @endsection
